d558223a870d38ad441469a23ff2412a9481853e
alexandre-kakal <alexandre.ls.kakal@gmail.com>
2022-07-13 10:05:27 +0200

    feat : add link to both versions

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 window.onload = () => {
   const canvas = document.getElementById("canvas1");
   const ctx = canvas.getContext("2d");
+  let inLink = false;
   ctx.canvas.width = window.innerWidth;
   ctx.canvas.height = window.innerHeight;
   let particleArray;
+  const linkText = "/matter.html";
 
   //create constructor function
-  function Particle(x, y, directionX, directionY, size, color) {
+  function Particle(x, y, directionX, directionY, size, color, text) {
     this.x = x;
     this.y = y;
     this.directionX = directionX;
     this.directionY = directionY;
     this.size = size;
     this.color = color;
+    this.text = text;
   }
 
   //add draw method to particle prototype
@@ -22,6 +25,7 @@ window.onload = () => {
     ctx.fillStyle = this.color;
     ctx.fill();
     ctx.stroke();
+    ctx.fillText("text", this.x, this.y, this.size);
     ctx.lineWidth = 5;
   };
 
@@ -43,15 +47,16 @@ window.onload = () => {
   function init() {
     particleArray = [];
     for (let i = 0; i < 10; i++) {
-      let size = 50;
+      let size = 40;
       let x = Math.random() * (innerWidth - size * 2);
       let y = Math.random() * (innerHeight - size * 2);
       let directionX = Math.random() * 0.4 - 0.2;
       let directionY = Math.random() * 0.4 - 0.2;
       let color = "white";
+      let text = "01";
 
       particleArray.push(
-        new Particle(x, y, directionX, directionY, size, color)
+        new Particle(x, y, directionX, directionY, size, color, text)
       );
     }
   }
@@ -62,6 +67,49 @@ window.onload = () => {
 
     for (let i = 0; i < particleArray.length; i++) {
       particleArray[i].update();
+    }
+
+    let linkX = particleArray[0].x,
+      linkY = particleArray[0].y,
+      linkWidth = 80,
+      linkHeight = 80;
+
+    canvas.addEventListener("mousemove", on_mousemove, false);
+    canvas.addEventListener("click", on_click, false);
+
+    //check if the mouse is over the link and change cursor style
+    function on_mousemove(ev) {
+      var x, y;
+
+      // Get the mouse position relative to the canvas element.
+      if (ev.layerX || ev.layerX == 0) {
+        //for firefox
+        x = ev.layerX;
+        y = ev.layerY;
+      }
+      x += 40;
+      y -= 40;
+
+      //is the mouse over the link?
+      if (
+        x >= linkX &&
+        x <= linkX + linkWidth &&
+        y <= linkY &&
+        y >= linkY - linkHeight
+      ) {
+        document.body.style.cursor = "pointer";
+        inLink = true;
+      } else {
+        document.body.style.cursor = "";
+        inLink = false;
+      }
+    }
+
+    //if the link has been clicked, go to link
+    function on_click() {
+      if (inLink) {
+        window.location = linkText;
+      }
     }
   }
 
--- sketch.js
+++ sketch.js
@@ -6,7 +6,8 @@ window.onload = () => {
     Bodies = Matter.Bodies,
     MouseConstraint = Matter.MouseConstraint,
     Mouse = Matter.Mouse,
-    Composite = Matter.Composite;
+    Composite = Matter.Composite,
+    Events = Matter.Events;
 
   // create an engine
   var engine = Engine.create(),
@@ -14,11 +15,9 @@ window.onload = () => {
   engine.world.gravity.y = 0;
   engine.world.gravity.scale = 0;
 
-  let link = "https://stackoverflow.com";
-  let linkText = "Professionnels";
-  var ctx = Render.context;
-  let linkWidth;
-  let inLink = false;
+  //constructor array output
+  let particleArray;
+  let vectorArray;
 
   // create a renderer
   var render = Render.create({
@@ -30,6 +29,13 @@ window.onload = () => {
       width: window.innerWidth,
       height: window.innerHeight,
     },
+  });
+
+  //resize canvas when window is resized
+  window.addEventListener("resize", function () {
+    render.canvas.attributes.nodeValue = window.innerWidth;
+    render.options.height = innerHeight;
+    Render.run(render);
   });
 
   // run the renderer
@@ -90,60 +96,124 @@ window.onload = () => {
 
   console.log("array", particleArray); */
 
-  const vector = Matter.Vector.create(
-    Math.random() * 0.1 - 0.05,
-    Math.random() * 0.005 - 0.00047
-  );
-  const vector1 = Matter.Vector.create(
-    Math.random() * 0.1 - 0.0045,
-    Math.random() * 0.00075 - 0.0025
-  );
-  const vector2 = Matter.Vector.create(
-    Math.random() * 0.1 - 0.05,
-    Math.random() * 0.005 - 0.00047
-  );
-  const vector3 = Matter.Vector.create(
-    Math.random() * 0.1 - 0.05,
-    Math.random() * 0.005 - 0.00047
-  );
+  let circleOptions = {
+    render: {
+      fillStyle: "#fff",
+      lineWidth: 8,
+      strokeStyle: "#000",
+      text: {
+        content: "xx",
+        color: "black",
+        size: 30,
+        family: "sans-serif",
+      },
+    },
+    friction: 0,
+    frictionAir: 0,
+    frictionStatic: 0,
+    restitution: 1,
+    inertia: Infinity,
+    inverseInertia: Infinity,
+    angularSpeed: 1,
+    angularVelocity: 1,
+  };
 
+  let staticOptions = {
+    isStatic: true,
+    restitution: 1,
+    friction: 0,
+    frictionAir: 0,
+    frictionStatic: 0,
+  };
+
+  console.log("world", world);
   console.log("render", render);
   // create two boxes and a ground
-  var circle1 = Bodies.circle(400, 200, 40, {
-    render: {
-      fillStyle: "#fff",
-      lineWidth: 8,
-      strokeStyle: "#000",
-      text: {
-        content: "01",
-        color: "black",
-        size: 30,
-        family: "sans-serif",
-      },
-    },
-    friction: 0,
-    frictionAir: 0,
-    frictionStatic: 0,
-  });
-  var circle2 = Bodies.circle(450, 50, 40, {
-    render: {
-      fillStyle: "#fff",
-      lineWidth: 8,
-      strokeStyle: "#000",
-      text: {
-        content: "02",
-        color: "black",
-        size: 30,
-        family: "sans-serif",
-      },
-    },
-    friction: 0,
-    frictionAir: 0,
-    frictionStatic: 0,
-  });
+
+  /*  function generateParticles(){
+    let particleArray = [];
+    let content = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"];
+    for ( let i =0; i<10; i++){
+      var circle[i]= Bodies.circle(
+        Math.random() * (innerWidth - 40 * 2),
+        Math.random() * (innerHeight - 40 * 2),
+        40,
+        circleOptions
+      );
+
+      particleArray.push(circle[i]);
+    }
+  } */
+
+  var circle1 = Bodies.circle(
+    Math.random() * (innerWidth - 40 * 2),
+    Math.random() * (innerHeight - 40 * 2),
+    40,
+    { ...circleOptions, link: "https://youtube.com" }
+  );
+  var circle2 = Bodies.circle(
+    Math.random() * (innerWidth - 40 * 2),
+    Math.random() * (innerHeight - 40 * 2),
+    40,
+    circleOptions
+  );
+
+  var circle3 = Bodies.circle(
+    Math.random() * (innerWidth - 40 * 2),
+    Math.random() * (innerHeight - 40 * 2),
+    40,
+    circleOptions
+  );
+
+  var circle4 = Bodies.circle(
+    Math.random() * (innerWidth - 40 * 2),
+    Math.random() * (innerHeight - 40 * 2),
+    40,
+    circleOptions
+  );
+  var circle5 = Bodies.circle(
+    Math.random() * (innerWidth - 40 * 2),
+    Math.random() * (innerHeight - 40 * 2),
+    40,
+    circleOptions
+  );
+  var circle6 = Bodies.circle(
+    Math.random() * (innerWidth - 40 * 2),
+    Math.random() * (innerHeight - 40 * 2),
+    40,
+    circleOptions
+  );
+  var circle7 = Bodies.circle(
+    Math.random() * (innerWidth - 40 * 2),
+    Math.random() * (innerHeight - 40 * 2),
+    40,
+    circleOptions
+  );
+
+  var circle8 = Bodies.circle(
+    Math.random() * (innerWidth - 40 * 2),
+    Math.random() * (innerHeight - 40 * 2),
+    40,
+    circleOptions
+  );
+
+  var circle9 = Bodies.circle(
+    Math.random() * (innerWidth - 40 * 2),
+    Math.random() * (innerHeight - 40 * 2),
+    40,
+    circleOptions
+  );
+
+  var circle10 = Bodies.circle(
+    Math.random() * (innerWidth - 40 * 2),
+    Math.random() * (innerHeight - 40 * 2),
+    40,
+    circleOptions
+  );
+
   var ground = Bodies.rectangle(
     window.innerWidth / 2,
-    window.innerHeight + 25,
+    window.innerHeight + 20,
     window.innerWidth,
     40,
     {
@@ -152,33 +222,27 @@ window.onload = () => {
   );
 
   var left = Bodies.rectangle(
-    -25,
+    -20,
     window.innerHeight / 2,
     40,
     window.innerHeight,
-    {
-      isStatic: true,
-    }
+    staticOptions
   );
 
   var right = Bodies.rectangle(
-    window.innerWidth + 25,
+    window.innerWidth + 20,
     window.innerHeight / 2,
     40,
     window.innerHeight,
-    {
-      isStatic: true,
-    }
+    staticOptions
   );
 
   var top = Bodies.rectangle(
     window.innerWidth / 2,
-    -25,
+    -20,
     window.innerWidth,
     40,
-    {
-      isStatic: true,
-    }
+    staticOptions
   );
 
   var pro = Bodies.rectangle(800, window.innerHeight - 200, 250, 60, {
@@ -194,20 +258,38 @@ window.onload = () => {
         family: "sans-serif",
       },
     },
+    restitution: 1,
+    friction: 0,
+    frictionAir: 0,
+    frictionStatic: 0,
   });
 
-  console.log("circle1", circle1);
   // add all of the bodies to the world
-  Composite.add(world, [circle1, circle2, ground, left, right, top, pro]);
+  Composite.add(world, [
+    circle1,
+    circle2,
+    circle3,
+    circle4,
+    circle5,
+    circle6,
+    circle7,
+    circle8,
+    circle9,
+    circle10,
+    ground,
+    left,
+    right,
+    top,
+    pro,
+  ]);
 
   //attempt to add mouse contraints and use the canvas onmousemove option in order to set links in the canva
 
-  /* // add mouse control
+  // add mouse control
   var mouse = Mouse.create(render.canvas),
     mouseConstraint = MouseConstraint.create(engine, {
       mouse: mouse,
       constraint: {
-        stiffness: 0.2,
         render: {
           visible: false,
         },
@@ -215,6 +297,11 @@ window.onload = () => {
     });
 
   Composite.add(world, mouseConstraint);
+
+  let canvasel = render.canvas;
+
+  console.log("canvas el", canvasel);
+  console.log("circle1", circle1);
 
   // keep the mouse in sync with rendering
   render.mouse = mouse;
@@ -225,16 +312,125 @@ window.onload = () => {
     max: { x: window.innerWidth, y: window.innerHeight },
   });
 
+  console.log("mouseConstraint", mouseConstraint);
+
   //Add event with 'mousemove'
-  Matter.Events.on(mouseConstraint, "mousemove", function (event) {
-    //For Matter.Query.point pass "array of bodies" and "mouse position"
-    var foundPhysics = Matter.Query.point(bodies, event.mouse.position);
+  //Matter.Events.on(mouseConstraint, "mousemove", function (event) {
+  //For Matter.Query.point pass "array of bodies" and "mouse position"
+  //var foundPhysics = Matter.Query.point(bodies, event.mouse.position);
 
-    //Your custom code here
-  }); */
+  //Your custom code here
+  //});
 
-  Matter.Body.applyForce(circle1, vector, vector1);
-  Matter.Body.applyForce(circle2, vector2, vector3);
+  /* class vector {
+    constructor(position, force) {
+      this.position = position;
+      this.force = force;
+    }
+  }
 
-  console.log("pro", pro);
+  function createVector() {
+    vectorArray = [];
+    for (let i = 0; i < 10; i++) {
+      let position = Math.random() * 0.01 - 0.02;
+      let force = Math.random() * 0.02 - 0.03;
+      vectorArray.push = new vector(position, force);
+    }
+  }
+
+  console.log("vectorArray", vectorArray);
+
+  createVector(); */
+
+  const vector0 = Matter.Vector.create(
+    Math.random() * 0.01 - 0.02,
+    Math.random() * 0.02 - 0.03
+  );
+  const vector1 = Matter.Vector.create(
+    Math.random() * 0.01 - 0.02,
+    Math.random() * 0.02 - 0.03
+  );
+  const vector2 = Matter.Vector.create(
+    Math.random() * 0.01 - 0.05,
+    Math.random() * 0.05 - 0.04
+  );
+  const vector3 = Matter.Vector.create(
+    Math.random() * 0.001 - 0.005,
+    Math.random() * 0.005 - 0.0047
+  );
+  const vector4 = Matter.Vector.create(
+    Math.random() * 0.001 - 0.002,
+    Math.random() * 0.002 - 0.003
+  );
+  const vector5 = Matter.Vector.create(
+    Math.random() * 0.001 - 0.002,
+    Math.random() * 0.002 - 0.003
+  );
+  const vector6 = Matter.Vector.create(
+    Math.random() * 0.001 - 0.005,
+    Math.random() * 0.005 - 0.004
+  );
+  const vector7 = Matter.Vector.create(
+    Math.random() * 0.001 - 0.005,
+    Math.random() * 0.005 - 0.003
+  );
+  const vector8 = Matter.Vector.create(
+    Math.random() * 0.001 - 0.002,
+    Math.random() * 0.002 - 0.003
+  );
+  const vector9 = Matter.Vector.create(
+    Math.random() * 0.001 - 0.002,
+    Math.random() * 0.002 - 0.003
+  );
+
+  function applyForceOnBodies() {
+    Matter.Body.applyForce(circle1, vector0, vector0);
+    Matter.Body.applyForce(circle2, vector1, vector1);
+    Matter.Body.applyForce(circle3, vector2, vector2);
+    Matter.Body.applyForce(circle4, vector3, vector3);
+    Matter.Body.applyForce(circle5, vector4, vector4);
+    Matter.Body.applyForce(circle6, vector5, vector5);
+    Matter.Body.applyForce(circle7, vector6, vector6);
+    Matter.Body.applyForce(circle8, vector7, vector7);
+    Matter.Body.applyForce(circle9, vector8, vector8);
+    Matter.Body.applyForce(circle10, vector9, vector9);
+  }
+
+  applyForceOnBodies();
+
+  setInterval(applyForceOnBodies, 15000);
+
+  Events.on(mouseConstraint, "mousedown", function (event) {
+    let ball = mouseConstraint.body;
+
+    if (!ball) return;
+    window.location = ball.link;
+  });
 };
+
+//how to set links using both mouse and bodies coordinates :
+//var ctx = render.canvas;
+let link = "/index.html";
+let linkWidth = 40;
+let linkHeight = 40;
+let inLink = false;
+
+//add mouse listeners
+
+//check if the mouse is over the link and change cursor style
+// get the mouse position relative to the canvas element
+// check if the mouse is over the link
+//true => inLink = true;
+//false => inlink = false;
+
+//if inLink true => go to the link : window.location = link;
+
+/*
+- synch the mouse position to the canva with onmousemove (almost done ? check mouse variable)
+- check if the mouse is over the link and change cursor style
+- if the link has been clicked, go to link
+
+for moving links (particles):
+- set x and y links coordinates equal to their particles coordinates
+
+*/
